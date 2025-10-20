4e3a6c6d519e2f5e435da31bdd8184dc61db01c7
AlexWork10 <alexcacereswork@gmail.com>
2025-10-20 19:08:56 +0200

    auth, guards, middlewares, resources, cors, swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,21 @@ async function bootstrap() {
     whitelist: true, // ignora campos extra
     forbidNonWhitelisted: true, // lanza error si hay campos extra
   }));
+
+  //configuracion Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // Habilitar CORS si es necesario
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [],
   controllers: [TasksController],
   providers: [TasksService],
 })
-export class TasksModule {}
+export class TasksModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(TasksController);
+  }
+}
